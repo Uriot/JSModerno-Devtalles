251b2310a28258de0969b7a2af7483b0fcfa6eb7
eurizar <elliotur@gmail.com>
2023-04-28 18:29:00 -0600

    Introduccion
    Clase de 1 - 11

---
+++ 01-fundamentos/app.js
@@ -0,0 +1,35 @@
+
+// console.log('Inicio de programa');
+
+let a   = 10,
+    b   = 20,
+    c   = 'Hola ',
+    d   = 'Spiderman',
+    x   = a + b + c;
+
+    const saludo = c + d;
+// var b   = 10; //? No se usa
+// const c = 10; //? No se puede reasignar
+
+// c = 10; //? No se puede reasignar
+
+//! mala paractica d = 11;
+
+//? polyfill codigo que procce funcionamiento moderno en versiones anteriores de JS
+
+// let x = a + b + c;
+// console.log( x );
+// console.warn( x );
+// console.error( x );
+
+// console.log( 'a', a  );
+// console.log( 'b', b  );
+// console.log( 'c', c );
+
+// console.log( { a } );
+// console.log( { b } );
+// console.log( { c } );
+
+// console.log( '%c Mis variables', 'color: blue; font-weight: bold' );
+
+// console.table( { a, b, c, d, x } );
